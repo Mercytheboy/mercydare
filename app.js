ab2a3a4d4edd39562ac6fbe0dd15929638e70ae2
Mercy Dare <daremercy1@gmail.com>
2023-03-02 17:41:01 +0100

    responsive portfolio webpage

---
+++ app.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".autotype-name", {
+        strings: ["Mercy Olamide Dare", "a frontend developer"],
+        typeSpeed: 150,
+        backSpeed: 75,
+        loop: true,
+      });
